d851e45a197b3f82776fed304fa77aca205a8149
chrisowen2015 <39311449+chrisowen2015@users.noreply.github.com>
2023-07-28 15:45:58 -0700

    Set up MUI theming with Next 13

---
+++ warm-wave/src/components/ThemeRegistry/theme.js
@@ -0,0 +1,55 @@
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const lightTheme = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+          main: '#FFA500'
+      },
+    },
+    typography: {
+      fontFamily: roboto.style.fontFamily,
+    },
+    components: {
+      MuiAlert: {
+        styleOverrides: {
+          root: ({ ownerState }) => ({
+            ...(ownerState.severity === 'info' && {
+              backgroundColor: '#60a5fa',
+            }),
+          }),
+        },
+      },
+    },
+  });
+
+  export const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+          main: '#FFA500'
+      },
+    },
+    typography: {
+      fontFamily: roboto.style.fontFamily,
+    },
+    components: {
+      MuiAlert: {
+        styleOverrides: {
+          root: ({ ownerState }) => ({
+            ...(ownerState.severity === 'info' && {
+              backgroundColor: '#60a5fa',
+            }),
+          }),
+        },
+      },
+    },
+  });
+
