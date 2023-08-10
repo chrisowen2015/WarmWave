d327ea5bf6aa995b30a8e6fb8d8d029fa6327b35
chrisowen2015 <39311449+chrisowen2015@users.noreply.github.com>
2023-08-09 17:48:38 -0700

    Created a mobile-friendly layout for Team page
    Changed the descriptions of the Team members and the photo for me

--- warm-wave/src/app/team/page.js
+++ warm-wave/src/app/team/page.js
@@ -1,5 +1,6 @@
 import Spacer from "@/components/spacer";
 import TeamPanel from "@/components/team-panel";
+import MobileTeamPanel from "@/components/Mobile/mobile-team-panel";
 import { Box, Paper, Tab, Tabs, Typography } from "@mui/material";
 
 export default function Team() {
@@ -10,31 +11,62 @@ export default function Team() {
             title: "Founder, Senior Mixing & Mastering Engineer",
             imgSrc: "/images/andrew-tyler-cropped.jpg",
             alt: "Andrew Tyler Image",
-            description: "Andrew Tyler hits bags and hits women."
+            description: "Andrew Tyler embarked on his journey in the music world as a musician and songwriter, based in a modest Santa Barbara apartment back in 2015. Recognizing his growing affinity for the technical aspects of music, he delved into the realms of mixing and mastering. Over time, he has collaborated with artists spanning the globe, utilizing his expertise to aid them in reaching their musical aspirations."
         },
         {
 
             name: "Chris Owen",
-            title: "Website Developer, Brand Owner",
-            imgSrc: "/images/chris-owen.png",
+            title: "Website Developer, Brand Developer",
+            imgSrc: "/images/chris-owen.jpg",
             alt: "Chris Owen Image",
-            description: "Chris also likes to hit bags but is gentle with the ladies."
+            description: "Chris Owen is a web developer and brand developer based in Pasadena, California. He has worked with a variety of clients, from small businesses to large corporations, to help them achieve their goals. He is passionate about helping others and is always looking for new ways to do so."
         }
     ];
 
     return (
-        <Box>
-            <Spacer height={200} />
+        <>
+            <Box sx={{
+                display: {
+                    xs: 'none',
+                    sm: 'none',
+                    md: 'block',
+                    lg: 'block',
+                    xl: 'block',
+                }
+            }}>
+                <Spacer height={200} />
 
-            <Typography variant="h1" component="h1" sx={{ textAlign: "center" }}>
-                Our Team
-            </Typography>
+                <Typography variant="h1" component="h1" sx={{ textAlign: "center" }}>
+                    Our Team
+                </Typography>
 
-            <Spacer height={50} />
+                <Spacer height={50} />
 
-            <TeamPanel team={team}/>
+                <TeamPanel team={team} />
 
-            <Spacer height={50} />
-        </Box>
+                <Spacer height={50} />
+            </Box>
+            <Box sx={{
+                display: {
+                    xs: 'block',
+                    sm: 'block',
+                    md: 'none',
+                    lg: 'none',
+                    xl: 'none',
+                }
+            }}>
+                <Spacer height={100} />
+
+                <Typography variant="h1" component="h1" sx={{ textAlign: "center" }}>
+                    Our Team
+                </Typography>
+
+                <Spacer height={25} />
+
+                <MobileTeamPanel team={team} />
+
+                <Spacer height={50} />
+            </Box>
+        </>
     )
 }
---
+++ warm-wave/src/components/Mobile/mobile-team-panel.js
@@ -0,0 +1,89 @@
+'use client';
+
+import Spacer from "@/components/spacer";
+import { Box, Divider, Paper, Tab, Tabs, Typography } from "@mui/material";
+import Image from "next/image";
+import { useState } from "react";
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div>
+            {value === index && (
+                <Box sx={{ p: 1 }}>
+                    {children}
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index) {
+    return {
+      id: `vertical-tab-${index}`,
+      'aria-controls': `vertical-tabpanel-${index}`,
+    };
+  }
+
+export default function MobileTeamPanel(props) {
+    const [value, setValue] = useState(0);
+    const team = props.team;
+
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+    return (
+        <Paper sx={{ padding: '1em', margin: '1em' }}>
+            <Box sx={{ display: 'block' }}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="Vertical tabs example"
+                >
+                    {
+                        team.map((teamMember, index) => (
+                            <Tab id={teamMember.name} key={index} label={teamMember.name} {...a11yProps(0)} />
+                        ))
+                    }
+
+                </Tabs>
+
+                <Divider />
+
+                {
+                    team.map((teamMember, index) => (
+                        <TabPanel key={teamMember.name} value={value} index={index}>
+                            <div id={teamMember.name} >
+                                <Box sx={{ display: 'block', padding: "1em 0 1em 0" }}>
+                                    <Image src={teamMember.imgSrc} style={{width: '100%', height: 'auto'}} width={600} height={450} alt={teamMember.alt} />
+                                    <Box sx={{ }}>
+                                        <Spacer height={10} />
+
+                                        <Typography variant="h4" component="h4">
+                                            {teamMember.name}
+                                        </Typography>
+
+                                        <Spacer height={10} />
+
+                                        <Typography variant="h5" component="h5">
+                                            {teamMember.title}
+                                        </Typography>
+
+                                        <Spacer height={25} />
+
+                                        <Typography variant="p" component="p">
+                                            {teamMember.description}
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                            </div>
+                        </TabPanel>
+                    ))
+                }
+            </Box>
+        </Paper>
+    );
+}
